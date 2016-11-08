5ab247c29a7456768f24ce5245a354b1a9eb9894
jez04 <david.jezek@vsb.cz>
2016-11-09 00:18:49 +0100

    Test commit added space

---
+++ com2eth/src/main/java/swi/dod/com2eth/Bridge.java
@@ -0,0 +1,26 @@
+package swi.dod.com2eth;
+
+/**
+ * Hello world!
+ *
+ */
+public class Bridge 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void listen(){
+    	
+    }
+    
+    public void com2eth(){
+    	
+    }
+    
+    public void eth2com(){
+    	
+    }
+    
+}
